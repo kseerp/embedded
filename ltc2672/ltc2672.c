03da26f539ef491ab8f0564dc296329e8597c654
Kim Seer Paller <kimseer.paller@analog.com>
2024-02-02 18:25:01 +0000

    ltc2672.c: refactor code
    make softpan bit as macro
    reduce code duplication

--- ltc2672/ltc2672.c
+++ ltc2672/ltc2672.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * LTC2672 12-/16 Bit Softspan DAC driver
+ * LTC2672 Current Output 16-Bit Softspan DAC driver
  *
- * Copyright 2023 Analog Devices Inc.
+ * Copyright 2024 Analog Devices Inc.
  */
 #include <linux/bitfield.h>
 #include <linux/bits.h>
@@ -23,54 +23,29 @@
 
 #define LTC2672_DAC_CHANNELS			5
 
-#define LTC2672_16BIT_FULL_SCALE		65535
+#define LTC2672_CMD_CH_CODE(x)			(0x00 + (x))
+#define LTC2672_CMD_CODE_PWRUP_UPD_CH(x)	(0x30 + (x))
+#define LTC2672_CMD_PWRDWN_CH(x)		(0x40 + (x))
+#define LTC2672_CMD_SPAN_TO_CH(x)		(0x60 + (x))
 
-#define LTC2672_ADDR_MASK                       GENMASK(3, 0)
-#define LTC2672_CMD_MASK                        GENMASK(7, 4)
+#define LTC2672_CMD_TOGGLE_SEL			0xC0
+#define LTC2672_CMD_TOGGLE_GLBL			0xD0
+
+// #define LTC2672_ADDR_MASK                       GENMASK(3, 0)
+// #define LTC2672_CMD_MASK                        GENMASK(7, 4)
+#define LTC2672_16BIT_MAX			65535
 // #define LTC2672_TOGGLE_SEL_MASK			GENMASK(4, 0)
 
-/*
- * Definition used because the latter is not available
- * in project's source tree.
- */
-#define IIO_DMA_MINALIGN	ARCH_KMALLOC_MINALIGN
 
-enum ltc2672_cmd {
-	LTC2672_CODE_TO_CHANNEL_X,
-	LTC2672_PWRUP_UPD_CHANNEL_X,
-	LTC2672_CODE_TO_CHANNEL_X_PWRUP_UPD_CHANNEL_ALL,
-	LTC2672_CODE_PWRUP_UPD_CHANNEL_X,
-	LTC2672_PWRDWN_CHANNEL_X,
-	LTC2672_PWRDWN_DEV,
-	LTC2672_SPAN_TO_CHANNEL_X,
-	LTC2672_CNFG_CMD,
-	LTC2672_CODE_TO_CHANNEL_ALL,
-	LTC2672_PWRUP_UPD_CHANNEL_ALL,
-	LTC2672_CODE_PWRUP_UPD_CHANNEL_ALL,
-	LTC2672_MON_MUX,
-	LTC2672_TOGGLE_SEL,
-	LTC2672_TOGGLE_GLBL,
-	LTC2672_SPAN_TO_CHANNEL_ALL,
-	LTC2672_NO_OP
-};
-
-/* Device Family */
-enum ltc2672_device_id {
-	LTC2672_12,
-	LTC2672_16
-};
-
-enum ltc2672_output_range {
-	LTC2672_OFF,
-	LTC2672_50VREF,
-	LTC2672_100VREF,
-	LTC2672_200VREF,
-	LTC2672_400VREF,
-	LTC2672_800VREF,
-	LTC2672_1600VREF,
-	LTC2672_3200VREF,
-	LTC2672_VMINUS_VREF,
-	LTC2672_4800VREF = 0XF
+enum {
+	LTC2672_SPAN_3_125,
+	LTC2672_SPAN_6_25,
+	LTC2672_SPAN_12_5,
+	LTC2672_SPAN_25,
+	LTC2672_SPAN_50,
+	LTC2672_SPAN_100,
+	LTC2672_SPAN_200,
+	LTC2672_SPAN_300,
 };
 
 enum {
@@ -78,79 +53,27 @@ enum {
 	LTC2672_INPUT_B,
 };
 
-enum {
-	LTC2672_TOGGLE_LOW,
-	LTC2672_TOGGLE_HIGH,
-};
-
-/* Multiplexer cmdand Codes */
-enum ltc2672_mux_cmd {
-	LTC2672_MUX_DISABLED,
-	LTC2672_MUX_IOUT0,
-	LTC2672_MUX_IOUT1,
-	LTC2672_MUX_IOUT2,
-	LTC2672_MUX_IOUT3,
-	LTC2672_MUX_IOUT4,
-	LTC2672_MUC_VCC,
-	LTC2672_MUX_VREF = 0x08,
-	LTC2672_MUX_VREF_LO,
-	LTC2672_MUX_DIE_TEMP,
-	LTC2672_MUX_VDD0 = 0x10,
-	LTC2672_MUX_VDD1,
-	LTC2672_MUX_VDD2,
-	LTC2672_MUX_VDD3,
-	LTC2672_MUX_VDD4,
-	LTC2672_MUX_VMINUS = 0X16,
-	LTC2672_MUX_GND,
-	LTC2672_MUX_VOUT0,
-	LTC2672_MUX_VOUT1,
-	LTC2672_MUX_VOUT2,
-	LTC2672_MUX_VOUT3,
-	LTC2672_MUX_VOUT4
-};
-
-/* Faults */
-enum ltc2672_faults {
-	LTC2672_OPEN_OUT0, // Open circuit CH0
-	LTC2672_OPEN_OUT1, // Open circuit CH0
-	LTC2672_OPEN_OUT2, // Open circuit CH0
-	LTC2672_OPEN_OUT3, // Open circuit CH0
-	LTC2672_OPEN_OUT4, // Open circuit CH0
-	LTC2672_OVER_TEMP, // Over-temperature (T > 175 deg C)
-	LTC2672_UNUSED, // Unused fault register bit
-	LTC2672_INV_LENGTH, // Invalid SPI Length (len != 24 or 32 * n)
+struct ltc2672_chan {
+	bool toggle;
+	bool powerdown;
+	u16 code;
+	u16 raw[2];
 };
 
 struct ltc2672_state {
-	struct spi_device       *spi;
-        struct regmap           *regmap;
-	struct mutex            lock;
-	struct regulator        *vref;
-	struct iio_chan_spec	*iio_chan;
-	int			vref_mv;
-	int			rfsadj;
-	u32			span_code[5];
-	u8			toggle_chan[5];
-	u8			toggle_glbl;
+	struct spi_device *spi;
+	struct regmap *regmap;
+	struct ltc2672_chan channels[LTC2672_DAC_CHANNELS];
+	struct mutex lock;
+	struct regulator *vref;
+	struct iio_chan_spec *iio_chan;
+	int vref_mv;
+	u32 span_code[5];
 };
 
-static int ltc2672_set_current(struct ltc2672_state *st, u8 addr, u16 val)
-{
-	u16 span, code;
-	int ret;
-
-	span = FIELD_PREP(LTC2672_ADDR_MASK, addr) |
-	       FIELD_PREP(LTC2672_CMD_MASK, LTC2672_SPAN_TO_CHANNEL_X);
-
-	code = FIELD_PREP(LTC2672_ADDR_MASK, addr) |
-	       FIELD_PREP(LTC2672_CMD_MASK, LTC2672_CODE_PWRUP_UPD_CHANNEL_X);
-
-	ret = regmap_write(st->regmap, span, st->span_code[addr]);
-	if (ret)
-		return ret;
-
-	return regmap_write(st->regmap, code, val);
-}
+static const int ltc2672_span_helper[] = {
+	3125, 6250, 12500, 25000, 50000, 100000, 200000, 300000,
+};
 
 static ssize_t ltc2672_dac_input_write(struct iio_dev *indio_dev,
 				       uintptr_t private,
@@ -158,42 +81,73 @@ static ssize_t ltc2672_dac_input_write(struct iio_dev *indio_dev,
 				       const char *buf, size_t len)
 {
 	struct ltc2672_state *st = iio_priv(indio_dev);
-	u16 val, code, toggle, span;
+	u16 val, toggle[5];
 	int ret;
-
-	span = FIELD_PREP(LTC2672_ADDR_MASK, chan->channel) |
-	       FIELD_PREP(LTC2672_CMD_MASK, LTC2672_SPAN_TO_CHANNEL_X);
-
-	code = FIELD_PREP(LTC2672_ADDR_MASK, chan->channel) |
-	       FIELD_PREP(LTC2672_CMD_MASK, LTC2672_CODE_TO_CHANNEL_X);
-
-	toggle = FIELD_PREP(LTC2672_CMD_MASK, LTC2672_TOGGLE_SEL);
-
-	if (private == LTC2672_INPUT_A)
-		st->toggle_chan[chan->channel] &= ~BIT(chan->channel);
-	else if (private == LTC2672_INPUT_B)
-		st->toggle_chan[chan->channel] |= BIT(chan->channel);
 
 	ret = kstrtou16(buf, 10, &val);
 	if (ret)
 		return ret;
 
-	if (val > LTC2672_16BIT_FULL_SCALE)
+	if (val > LTC2672_16BIT_MAX || val < 0)
 		return -EINVAL;
 
-	ret = regmap_write(st->regmap, span, st->span_code[chan->channel]);
+	if (private == LTC2672_INPUT_A)
+		toggle[chan->channel] &= ~BIT(chan->channel);
+	else
+		toggle[chan->channel] |= BIT(chan->channel);
+
+	ret = regmap_write(st->regmap, LTC2672_CMD_TOGGLE_SEL,
+			   toggle[chan->channel]);
 	if (ret)
 		return ret;
 
-	ret = regmap_write(st->regmap, toggle, st->toggle_chan[chan->channel]);
-	if (ret)
-		return ret;
-
-	ret = regmap_write(st->regmap, code, val);
+	ret = regmap_write(st->regmap, LTC2672_CMD_CH_CODE(chan->channel), val);
 	if (ret)
 		return ret;
 
 	return ret ?: len;
+}
+
+static ssize_t ltc2672_dac_input_read(struct iio_dev *indio_dev,
+				      uintptr_t private,
+				      const struct iio_chan_spec *chan,
+				      char *buf)
+{
+	// struct ltc2672_state *st = iio_priv(indio_dev);
+
+	// return sysfs_emit(buf, "%d\n", st->toggle_chan[chan->channel]);
+	return 0;
+}
+
+static ssize_t ltc2672_toggle_set(struct iio_dev *indio_dev,
+				    uintptr_t private,
+				    const struct iio_chan_spec *chan,
+				    const char *buf, size_t len)
+{
+	struct ltc2672_state *st = iio_priv(indio_dev);
+	bool en;
+	int ret;
+
+	ret = kstrtobool(buf, &en);
+	if (ret)
+		return ret;
+
+	ret = regmap_write(st->regmap, private, en);
+	if (ret)
+		return ret;
+
+	return ret ?: len;
+}
+
+static ssize_t ltc2672_toggle_get(struct iio_dev *indio_dev,
+				    uintptr_t private,
+				    const struct iio_chan_spec *chan,
+				    char *buf)
+{
+	// struct ltc2672_state *st = iio_priv(indio_dev);
+
+	// return sysfs_emit(buf, "%d\n", st->toggle_glbl);
+	return 0;
 }
 
 static ssize_t ltc2672_powerdown_set(struct iio_dev *indio_dev,
@@ -202,76 +156,18 @@ static ssize_t ltc2672_powerdown_set(struct iio_dev *indio_dev,
 				     const char *buf, size_t len)
 {
 	struct ltc2672_state *st = iio_priv(indio_dev);
-	u16 code;
 	bool en;
 	int ret;
-
-	code = FIELD_PREP(LTC2672_ADDR_MASK, chan->channel) |
-	       FIELD_PREP(LTC2672_CMD_MASK, LTC2672_PWRDWN_CHANNEL_X);
 
 	ret = kstrtobool(buf, &en);
 	if (ret)
 		return ret;
 
-	mutex_lock(&st->lock);
-
-	ret = regmap_write(st->regmap, code, en);
+	ret = regmap_write(st->regmap, LTC2672_CMD_PWRDWN_CH(chan->channel), en);
 	if (ret)
-		goto out_unlock;
-
-out_unlock:
-	mutex_unlock(&st->lock);
+		return ret;
 
 	return len;
-}
-
-static ssize_t ltc2672_dac_input_read(struct iio_dev *indio_dev,
-				      uintptr_t private,
-				      const struct iio_chan_spec *chan,
-				      char *buf)
-{
-	struct ltc2672_state *st = iio_priv(indio_dev);
-
-	return sysfs_emit(buf, "%d\n", st->toggle_chan[chan->channel]);
-}
-
-static ssize_t ltc2672_toggle_set(struct iio_dev *indio_dev,
-				  uintptr_t private,
-				  const struct iio_chan_spec *chan,
-				  const char *buf, size_t len)
-{
-	struct ltc2672_state *st = iio_priv(indio_dev);
-	u16 toggle;
-	bool en;
-	int ret;
-
-	toggle = FIELD_PREP(LTC2672_CMD_MASK, LTC2672_TOGGLE_GLBL);
-
-	ret = kstrtobool(buf, &en);
-	if (ret)
-		return ret;
-
-	mutex_lock(&st->lock);
-	st->toggle_glbl = en;
-
-	ret = regmap_write(st->regmap, toggle, en);
-	if (ret)
-		goto out_unlock;
-
-out_unlock:
-	mutex_unlock(&st->lock);
-
-	return ret ?: len;
-}
-
-static ssize_t ltc2672_toggle_get(struct iio_dev *indio_dev,
-				  uintptr_t private,
-				  const struct iio_chan_spec *chan,
-				  char *buf)
-{
-	struct ltc2672_state *st = iio_priv(indio_dev);
-
-	return sysfs_emit(buf, "%d\n", st->toggle_glbl);
 }
 
 static int ltc2672_write_raw(struct iio_dev *indio_dev,
@@ -282,7 +178,12 @@ static int ltc2672_write_raw(struct iio_dev *indio_dev,
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		return ltc2672_set_current(st, chan->channel, val);
+		if (val > LTC2672_16BIT_MAX || val < 0)
+			return -EINVAL;
+
+		return regmap_write(st->regmap,
+			            LTC2672_CMD_CODE_PWRUP_UPD_CH(chan->channel),
+				    val);
 	default:
 		return -EINVAL;
 	}
@@ -313,7 +214,7 @@ static const struct iio_chan_spec_ext_info toggle_mode_ext_info[] = {
 			      ltc2672_dac_input_read, ltc2672_dac_input_write),
 	LTC2672_CHAN_EXT_INFO("raw1", LTC2672_INPUT_B, IIO_SEPARATE,
 			      ltc2672_dac_input_read, ltc2672_dac_input_write),
-	LTC2672_CHAN_EXT_INFO("toggle", 0, IIO_SEPARATE,
+	LTC2672_CHAN_EXT_INFO("toggle", LTC2672_CMD_TOGGLE_GLBL, IIO_SEPARATE,
 			      ltc2672_toggle_get, ltc2672_toggle_set),
 	LTC2672_CHAN_EXT_INFO("powerdown", 0, IIO_SEPARATE,
 			      NULL, ltc2672_powerdown_set),
@@ -348,13 +249,13 @@ static int ltc2672_chan_config(struct ltc2672_state *st)
 {
 	struct device *dev = &st->spi->dev;
 	struct fwnode_handle *child;
-	u32 reg;
-	int ret;
+	u32 reg, span_range, span_code[5];
+	int ret, span;
 
-	ret = device_property_read_u32(dev, "adi,rfsadj-ohms", &st->rfsadj);
-	if (ret)
-		return dev_err_probe(dev, ret,
-				     "adi,rfsadj property not found\n");
+	// ret = device_property_read_u32(dev, "adi,rfsadj-ohms", &st->rfsadj);
+	// if (ret)
+	// 	return dev_err_probe(dev, ret,
+	// 			     "adi,rfsadj property not found\n");
 
 	device_for_each_child_node(dev, child) {
 		ret = fwnode_property_read_u32(child, "reg", &reg);
@@ -370,25 +271,36 @@ static int ltc2672_chan_config(struct ltc2672_state *st)
 					     "reg property out of range\n");
 		}
 
-		if (fwnode_property_read_bool(child, "adi,toggle-mode"))
-			st->iio_chan[reg].ext_info = toggle_mode_ext_info;
-
-		st->span_code[reg] = LTC2672_50VREF;
-		ret = fwnode_property_read_u32(child, "adi,current-span-code", &st->span_code[reg]);
-		if (ret) {
-			fwnode_handle_put(child);
-			return dev_err_probe(dev, ret,
-					     "adi,current-span-code property not found\n");
+		ret = fwnode_property_read_u32(child, "adi,output-range-microamp",
+					       &span_range);
+		if (!ret) {
+			for (span = 0 ; span < ARRAY_SIZE(ltc2672_span_helper); span++) {
+				if (ltc2672_span_helper[span] == span_range / 1000)
+					span_code[reg] = span + 1;
+			}
 		}
 
-		if (st->span_code[reg] > LTC2672_VMINUS_VREF &&
-		    st->span_code[reg] != LTC2672_4800VREF) {
+		if (span_code[reg] < 0) {
 			fwnode_handle_put(child);
 			return dev_err_probe(dev, -EINVAL,
-					     "adi,current-span-code property out of range\n");
+					     "adi,output-range-microvolt property out of range\n");
 		}
-		
-		printk(KERN_INFO "span_code[%d] = %d\n", reg, st->span_code[reg]);
+
+		ret = regmap_write(st->regmap, LTC2672_CMD_SPAN_TO_CH(reg),
+				   span_code[reg]);
+		if (ret) {
+			fwnode_handle_put(child);
+			return ret;
+		}
+
+		if (fwnode_property_present(child, "adi,toggle-mode")) {
+			st->iio_chan[reg].ext_info = toggle_mode_ext_info;
+			/*
+			 * Clear IIO_CHAN_INFO_RAW bit as toggle channels expose
+			 * out_voltage_raw{0|1} files.
+			 */
+			__clear_bit(IIO_CHAN_INFO_RAW, &st->iio_chan[reg].info_mask_separate);
+		}
 	}
 
 	return 0;
@@ -467,7 +379,7 @@ static int ltc2672_probe(struct spi_device *spi)
 		return dev_err_probe(dev, PTR_ERR(reset),
 				     "Failed to get reset gpio\n");
 	if (reset) {
-		msleep(100);
+		ndelay(30);
 		gpiod_set_value_cansleep(reset, 1);
 	}
 
